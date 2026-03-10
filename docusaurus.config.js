32cbb9e74318672d04591650c3b4b2f5b9a55474
Ilian Sapundshiev <ilian@tagspaces.org>
2026-03-10 21:59:23 +0100

    added lastmod date to sitemap.xml

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -328,6 +328,7 @@ const config = {
         sitemap: {
           changefreq: "weekly",
           priority: 0.5,
+          lastmod: "date",
           // trailingSlash: true,
         },
       }),
